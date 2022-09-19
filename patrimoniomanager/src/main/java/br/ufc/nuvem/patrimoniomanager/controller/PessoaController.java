8e98eeec697b9c4e93cb1fd62803b3cff570cbc1
Victor Misael <misael1805@gmail.com>
2022-09-19 13:54:46 -0300

    Faz crud de Pessoa

--- patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/controller/PessoaController.java
+++ patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/controller/PessoaController.java
@@ -7,10 +7,10 @@ import br.ufc.nuvem.patrimoniomanager.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +21,26 @@ public class PessoaController {
     @PostMapping
     public ResponseEntity<Pessoa> insertPessoa(@RequestParam PessoaDTO pessoaDTO) {
         return new ResponseEntity<>(pessoaService.save(pessoaDTO.toPessoa()), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> getAllPessoas() {
+        return new ResponseEntity<>(pessoaService.find(), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Pessoa>> getPessoa(@PathVariable Long id) {
+        return new ResponseEntity<>(pessoaService.find(id), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletePessoa(@RequestParam Long id) {
+        pessoaService.delete(id);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Pessoa> updatePessoa(Pessoa pessoa) {
+        return new ResponseEntity<>(pessoaService.updatePessoa(pessoa), HttpStatus.ACCEPTED);
     }
 }
--- patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/service/PessoaService.java
+++ patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/service/PessoaService.java
@@ -3,6 +3,12 @@ package br.ufc.nuvem.patrimoniomanager.service;
 import br.ufc.nuvem.patrimoniomanager.model.Pessoa;
 import br.ufc.nuvem.patrimoniomanager.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class PessoaService {
@@ -10,19 +16,34 @@ public class PessoaService {
     private final S3Service s3Service;
     private final PessoaRepository pessoaRepository;
 
-    public Pessoa save(Pessoa pessoa){
+    public Pessoa save(Pessoa pessoa) {
         return pessoaRepository.save(pessoa);
     }
 
-    public void find(Long id){
-        pessoaRepository.findById(id);
+    public Optional<Pessoa> find(Long id) {
+       return pessoaRepository.findById(id);
     }
 
-    public void delete(Pessoa pessoa){
-        pessoaRepository.delete(pessoa);
-    };
+    public List<Pessoa> find() {
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoaRepository.findAll().forEach(pessoas::add);
+        return pessoas;
+    }
 
-    public void insertPhoto(Pessoa pessoa){
+
+    public void delete(Long id) {
+        pessoaRepository.deleteById(id);
+    }
+
+    public Pessoa updatePessoa(Pessoa pessoa) {
+        if (pessoaRepository.existsById(pessoa.getId())) {
+            return pessoaRepository.save(pessoa);
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Update without ID");
+
+    }
+
+    public void insertPhoto(Pessoa pessoa) {
 
     }
 

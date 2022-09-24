44a4e9a0cf811f0aa718ebf3f3d1ce32460e1088
andersonluiss <anderson.luis@techwayinfo.com.br>
2022-09-24 17:27:52 -0300

    S3 implementado

--- patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/strategy/S3StorageStrategy.java
+++ patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/strategy/S3StorageStrategy.java
@@ -1,22 +1,15 @@
 package br.ufc.nuvem.patrimoniomanager.strategy;
 
+
 import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.ec2.model.S3Storage;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -25,7 +18,7 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 
 @Component
-public class S3StorageStrategy implements StorageStrategy {
+public class S3StorageStrategy implements StorageStrategy{
 
     private final String S3BucketName;
     private final AmazonS3 s3;
@@ -52,22 +45,52 @@ public class S3StorageStrategy implements StorageStrategy {
     }
 
     @Override
-    public boolean insertFileAtFolder(String foldername, File file) {
-        return false;
+    public String insertFileAtFolder(String foldername, File file) {
+        //Realiza a inserção do arquivo e retorna a url para salvamento no banco
+        try {
+            s3.putObject(new PutObjectRequest(S3BucketName ,foldername + "/" + file.getName(), file ));
+            return getObjectReference(foldername + "/" + file.getName());
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return "";
+        }
     }
 
     @Override
     public boolean deleteFile(String filename) {
-        return false;
+        String urlReference = getObjectReference(filename);
+        //Se o arquivo existe no folder, realiza a exclusão
+        if (!urlReference.equals("")){
+            s3.deleteObject(S3BucketName, filename);
+            return true;
+        }else {
+
+            return false;
+        }
+    }
+    @Override
+    public String getObjectReference(String filename){
+        //Obtem a lista de arquivos do folder especificado
+        ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
+                .withBucketName(S3BucketName)
+                .withPrefix(filename.split("/")[0]));
+
+        //Verifica se o arquivo em questao está presente no folder
+
+        for (S3ObjectSummary s3Summary:objectListing.getObjectSummaries())
+        {
+             if (s3Summary.getKey().equals(filename)){
+                //Caso o folder possua o arquivo, é retornada a url.
+                return s3.getUrl(S3BucketName,filename).toString();
+            }
+        }
+        //Caso o arquivo nao seja encontrado, retorna branco
+        return "";
+
+
+
+
+
     }
 
-    @Override
-    public String getObjectReference(String filename) {
-        return null;
-    }
-
-    @Override
-    public String deleteObject(String object) {
-        return null;
-    }
 }
--- patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/strategy/StorageStrategy.java
+++ patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/strategy/StorageStrategy.java
@@ -3,11 +3,11 @@ package br.ufc.nuvem.patrimoniomanager.strategy;
 import java.io.File;
 
 public interface StorageStrategy {
-    public boolean insertFileAtFolder(String foldername, File file);
+    public String insertFileAtFolder(String foldername, File file);
 
     public boolean deleteFile(String filename);
 
     public String getObjectReference(String filename);
 
-    public String deleteObject(String object);
+
 }

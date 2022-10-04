e0c94c08b5181d0cb1c4c73b10b13c619c485e29
Victor Misael <misael1805@gmail.com>
2022-10-04 12:29:51 -0300

    teste cors

--- patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/configuration/security/SecurityConfig.java
+++ patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/configuration/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,17 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-
 
 
     @Bean
@@ -34,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //
-        http
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/alive/**", "/auth/**", "/bem/{id}", "/validation/**")
                 .permitAll()
@@ -54,6 +50,7 @@ public class SecurityConfig {
                 .httpBasic();
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors(Customizer.withDefaults());
         return http.build();
     }
 

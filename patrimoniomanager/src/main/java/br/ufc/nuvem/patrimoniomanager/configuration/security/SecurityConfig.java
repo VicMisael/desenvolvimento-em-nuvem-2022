2cc33904c9a9e72699ff5df50158bfed2974312e
Victor Misael <misael1805@gmail.com>
2022-10-04 12:25:20 -0300

    teste cors

--- patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/configuration/security/SecurityConfig.java
+++ patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/configuration/security/SecurityConfig.java
@@ -6,15 +6,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //
-        http.cors().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/alive/**", "/auth/**", "/bem/{id}", "/validation/**")
                 .permitAll()
@@ -36,12 +46,15 @@ public class SecurityConfig {
                 .hasAnyAuthority("ROOT")
                 .antMatchers(HttpMethod.DELETE, "/user")
                 .hasAuthority("ROOT")
-                .antMatchers(HttpMethod.GET,"/bem")
+                .antMatchers(HttpMethod.GET, "/bem")
                 .permitAll()
                 .antMatchers("/bem/**")
                 .hasAnyAuthority("USER", "ROOT")
                 .and()
                 .httpBasic();
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
+
 }

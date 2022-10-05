6cb65047dde9ed4869e0717185c66f48859effef
Victor Misael <misael1805@gmail.com>
2022-10-04 22:30:35 -0300

    add security

--- patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/model/DTO/BemEditDTO.java
+++ patrimoniomanager/src/main/java/br/ufc/nuvem/patrimoniomanager/model/DTO/BemEditDTO.java
@@ -1,9 +1,13 @@
 package br.ufc.nuvem.patrimoniomanager.model.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BemEditDTO {
 
     @NonNull
